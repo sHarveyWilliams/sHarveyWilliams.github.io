2b1ac90c26d8fb48a5a529a13133f011de9edbc5
Yaroslav Maximov <sHarveyWilliams@gmail.com>
2019-04-28 16:43:32 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 document.addEventListener('DOMContentLoaded', Init);
 
-function getHttp(url = 'https://api.punkapi.com/v2/beers?per_page=8&page=1') {
+let arrayList = [];
+let arrayFilterList = [];
+let objListFilter = {};
+
+function getData(numberPage) {
+    numberPage = numberPage || 1;
+    url = `https://api.punkapi.com/v2/beers?per_page=8&page=${numberPage}`;
+
     return fetch(url, {
         method: 'GET',
         headers: {
@@ -14,90 +21,99 @@ function getHttp(url = 'https://api.punkapi.com/v2/beers?per_page=8&page=1') {
         .catch(error => console.error('Error:', error))
 }
 
-function renderList() {
-    getHttp()
+function getCheckedFromLS() {
+    let key;
+
+    for (let i = 0; i < window.localStorage.length; i++) {
+        key = localStorage.key(i);
+        objListFilter[key] = localStorage.getItem(key);
+    }
+    //console.log(objListFilter)
+}
+
+function createDOMElements(array, listElements) {
+    listElements = listElements || document.getElementById('list');
+    for (let i = 0; i < array.length; i++) {
+        const itemBeer = document.createElement('div');
+        const titleElm = document.createElement('div');
+        const descriptionElm = document.createElement('div');
+        const alcElm = document.createElement('div');
+        const imageElm = document.createElement('img');
+        const checkBox = document.createElement('input');
+        //console.log(array)
+        itemBeer.setAttribute('class', 'itemBeer');
+        //itemBeer.setAttribute('id', `itemBeer${i}`);
+
+        titleElm.setAttribute('class', 'title');
+
+        descriptionElm.setAttribute('class', 'descr');
+        titleElm.innerText = array[i]['name'];
+
+        descriptionElm.innerText = array[i]['description'];
+
+        imageElm.setAttribute('src', array[i]['image_url']);
+        alcElm.innerText = array[i]['abv'];
+
+        if (objListFilter[titleElm.innerText]) {
+            checkBox.setAttribute('checked', '');
+        }
+
+        checkBox.setAttribute('type', 'checkbox');
+        checkBox.setAttribute('class', 'checkBoxes');
+
+        itemBeer.appendChild(titleElm);
+        itemBeer.appendChild(imageElm);
+        itemBeer.appendChild(descriptionElm);
+        itemBeer.appendChild(checkBox);
+        itemBeer.appendChild(alcElm);
+        listElements.appendChild(itemBeer);
+    }
+}
+
+function renderList(numberPage) {
+    arrayList = [];
+
+    getData(numberPage)
         .then(data => {
-            const list = document.getElementById('list');
+            data.forEach((item) => {
+                arrayList.push(item)
+            });
 
-            for (let i = 0; i < data.length; i++) {
-
-                const itemBeer = document.createElement('div');
-                itemBeer.setAttribute('class', 'itemBeer');
-                itemBeer.setAttribute('id', `itemBeer${i}`);
-                if (document.getElementById(`itemBeer${i}`)) {
-                    continue;
-                } else {
-                    const titleElm = document.createElement('div');
-                    titleElm.setAttribute('class', 'title');
-
-                    const descriptionElm = document.createElement('div');
-                    descriptionElm.setAttribute('class', 'descr');
-
-                    const alcElm = document.createElement('div');
-
-                    const imageElm = document.createElement('img');
-
-                    titleElm.innerText = getInfoBeer(data[i], 'name');
-                    descriptionElm.innerText = getInfoBeer(data[i], 'description');
-                    imageElm.setAttribute('src', getInfoBeer(data[i], 'image_url'));
-                    alcElm.innerText = getInfoBeer(data[i], 'abv');
-
-
-                    const checkBox = document.createElement('input');
-                    checkBox.setAttribute('type', 'checkbox');
-                    checkBox.setAttribute('class', 'checkBoxes');
-
-                    if (localStorage.getItem(titleElm.innerText)) {
-                        //let check = localStorage.getItem(titleElm.innerText);
-                        checkBox.setAttribute('checked', '');
-                    }
-
-                    itemBeer.appendChild(titleElm);
-                    itemBeer.appendChild(imageElm);
-                    itemBeer.appendChild(descriptionElm);
-                    itemBeer.appendChild(checkBox);
-                    itemBeer.appendChild(alcElm);
-                    list.appendChild(itemBeer);
-
-                }
-            }
-
+            createDOMElements(arrayList);
         })
-        .catch(error => console.error('Error:', error))
 }
 
-function getInfoBeer(item, key) {
-    if (key === 'name' || key === 'description' || key === 'image_url' || key === 'abv') {
-        return item[key];
+function clearList(listElements) {
+    listElements.innerHTML = '';
+}
+
+function createPagination(event, listElements, paganationBar) {
+    paganationBar.innerHTML = '';
+    clearList(listElements);
+    renderList(parseInt(event.innerText));
+    showNumbersOfPages(paganationBar, +event.innerText);
+}
+
+function showNumbersOfPages(paganationBar, page) {
+    let lastPage;
+
+    updateUrl(page);
+
+    if (page % 10 > 7 || page % 10 === 0) { //Вопрос со страницами
+        lastPage = page + 10;
     }
-}
 
-function checkBoxListener(eventBox) {
-    if (eventBox.className != 'checkBoxes') {
-        return;
-    } else {
-        const titleElm = eventBox.parentElement.children[0].innerHTML;
-        if (!eventBox.getAttribute('checked', '')) {
-            addToLocalStorage(titleElm, 'checked');
-            updateList()
-            renderList();
-        }
+    if (page % 10 < 7 && page > 20) {
+        lastPage = page - 10;
     }
-}
 
-function updateList() { // проверка на checked
-    const data = document.getElementById('list');
+    for (page; page < lastPage; page++) {
+        const numberOfPage = document.createElement('td');
 
-    for (let i = 0; i < data.children.length; i++) {
-        if (localStorage.getItem(data.children[i].children[0].innerText)) {
-            data.children[i].children[3].setAttribute('checked', '');
-        }
-    }
-}
+        numberOfPage.setAttribute('class', 'numberOfPage');
+        numberOfPage.innerHTML = page;
+        paganationBar.appendChild(numberOfPage);
 
-function addToLocalStorage(title, checkbox) {
-    if (title) {
-        localStorage.setItem(title, (JSON.stringify(checkbox)));
     }
 }
 
@@ -105,6 +121,7 @@ function checkAuth() { //проверяем данные для аутентиф
     const name = document.getElementById('nameInput');
     const email = document.getElementById('emailInput');
     const pass = document.getElementById('passwordInput');
+
     if (pass.value.length < 8) {
         alert('Пароль должен быть больше 8 симоволов');
         pass.value = '';
@@ -116,49 +133,68 @@ function checkAuth() { //проверяем данные для аутентиф
     }
 }
 
-function sortElements(event, listElements, noteAtribute) {
-    let arr = [];
+function sortABVElements(listElements) {
+    arrayList.sort((elmA, elmB) => {
+        return +elmA['abv'] - +elmB['abv'];
+    });
 
-    if (noteAtribute === 'title') {
-        noteAtribute = 0;
-    }
-    if (noteAtribute === 'alcohol') {
-        noteAtribute = 4;
-    }
-
-    for (let i = 0; i < listElements.children.length; i++) {
-
-        arr.push(listElements.children[i].children[noteAtribute].innerHTML);
-    }
-
-    arr = sortNameOrNumbers(arr, noteAtribute);
-    replaceElements(listElements, arr, noteAtribute)
+    clearList(listElements);
+    createDOMElements(arrayList);
 }
 
-function sortNameOrNumbers(arr, noteAtribute) { //вызываем для различных сортировок
-    if (noteAtribute === 0) {
-        return arr.sort();
-    }
-    if (noteAtribute === 4) {
-        return arr.sort((elmA, elmB) => {
-            return elmA - elmB;
-        });
+
+function sortTitleElements(listElements) {
+    arrayList.sort((elmA, elmB) => {
+        let titleA = elmA['name'].toLowerCase();
+        let titleB = elmB['name'].toLowerCase();
+
+        if (titleA < titleB) {
+            console.log(1);
+            return -1;
+        }
+        if (titleA > titleB) {
+            return 1;
+        }
+        return 0;
+    });
+    clearList(listElements);
+    createDOMElements(arrayList);
+}
+
+function checkBoxListener(eventBox) {
+    if (eventBox.className != 'checkBoxes') {
+        return;
+    } else {
+        checkBoxAction(eventBox.parentElement.children[0].innerHTML);
     }
 }
 
-function replaceElements(listElements, arr, noteAtribute) { //вызываем для расстановки в соответствии с массивом
-    for (let i = 0; i < listElements.children.length; i++) {
-        for (let j = 0; j < arr.length; j++) {
-            if (listElements.children[j].children[noteAtribute].innerHTML === arr[i]) {
-                listElements.appendChild(listElements.children[j]);
-            }
+function checkBoxAction(nameElm) {
+    if (!objListFilter[nameElm]) {
+        localStorage.setItem(nameElm, 'checked');
+        objListFilter[nameElm] = 'checked'
+    } else {
+        delete objListFilter[nameElm];
+        localStorage.removeItem(nameElm);
+    }
+}
+
+function findChecked() {
+    console.log(objListFilter)
+    for (let i = 0; i < arrayList.length; i++) {
+        if (objListFilter[arrayList[i]['name']]) {
+            arrayFilterList.push(arrayList[i]);
         }
     }
+}
 
+function addElementsInModal(modalWindow) {
+    findChecked();
+    createDOMElements(arrayFilterList, modalWindow);
 }
 
 function showModalWindow(listElements) {
-    if(!document.getElementById('modal')) {
+    if (!document.getElementById('modal')) {
         const modalWindow = document.createElement('div');
         const modalOverlay = document.createElement('div');
         const exitModalWindow = document.createElement('div');
@@ -171,88 +207,60 @@ function showModalWindow(listElements) {
         document.body.appendChild(modalWindow);
         document.body.appendChild(modalOverlay);
 
-        document.getElementById('exitModal').addEventListener('click', event => clearModal(modalWindow, modalOverlay));
-        document.getElementById('modal-overlay').addEventListener('click', event => clearModal(modalWindow, modalOverlay));
-        addPagination(listElements, modalWindow);
+        document.getElementById('exitModal').addEventListener('click', event => clearModal(modalWindow, modalOverlay, listElements));
+        document.getElementById('modal-overlay').addEventListener('click', event => clearModal(modalWindow, modalOverlay, listElements));
+
+        addElementsInModal(modalWindow);
     }
 }
 
-function addPagination(listElements, modalWindow) {
-    const paganation = document.createElement('div');
-    paganation.setAttribute('id', 'pgnt');
-
-    generateElements(paganation, listElements, modalWindow);
-}
-
-function generateElements(pagination, listElements, modalWindow) {
-    let arrPag = [];
-    const curs = document.createElement('div');
-    curs.setAttribute('id', 'curs');
-    curs.setAttribute('class', 'curs');
-
-    const modalItem = document.createElement('div');
-    modalItem.setAttribute('class', 'itemBeerModal');
-
-    for (let i = 0; i < listElements.children.length; i++) {
-        let checkElm = listElements.children[i].children[3].getAttribute('checked');
-        if (checkElm === '') {
-            arrPag.push(listElements.children[i]);
-        }
-    }
-    for (let i = 0; i < arrPag.length; i++) {
-        const elmCurs = document.createElement('td');
-        elmCurs.innerHTML = i;
-        curs.appendChild(elmCurs);
-    }
-    pagination.appendChild(curs);
-
-    modalWindow.appendChild(pagination);
-    modalWindow.appendChild(modalItem);
-    document.body.appendChild(modalWindow); //в модальное окно пагинацию??????? //Добавляем
-
-    document.getElementById('curs').addEventListener('click', event => showElmOnPaganation(event.target, modalWindow, arrPag, modalItem))
-}
-
-function clearLocalStorage() {
-    localStorage.clear();
-    updateList();
-}
-
-function showElmOnPaganation(event, modalWindow, arrPag, modalItem) { // алгоритм пагинации
-    if (event) {
-        modalItem.innerHTML = '';
-        for (let i = 0; i < arrPag.length; i++) {
-            for (let j = 0; j < arrPag.length; j++) {
-                if (parseInt(event.innerHTML) === j) {
-                    arrPag[j].removeAttribute('class'); //АВТОМАТИЧЕСКОЕ ПЕРЕМЕЩЕНИЕ ЭЛЕМЕНТОВ
-                    modalItem.appendChild(arrPag[j]);
-                    modalWindow.appendChild(modalItem);
-                    renderList();
-                }
-            }
-        }
-    }
-}
-
-function clearModal(modalWindow, modalOverlay) { //кнопка выхода
-    modalWindow.outerHTML='';
+function clearModal(modalWindow, modalOverlay, listElements) { //кнопка выхода
+    const numberPage = getURLIdPage();
+    modalWindow.outerHTML = '';
     modalOverlay.outerHTML = '';
+    clearList(listElements);
+    arrayFilterList=[];
+    createDOMElements(arrayList, numberPage);
 }
 
-function clearList(listElements) {
-    listElements.innerHTML = '';
-    renderList();
-    clearLocalStorage();
+function updateUrl(numberPage) {
+    const url = new URL(window.location.origin + "/" + "index.html");
+
+    if (history.pushState) {
+        history.pushState(null, null, url + `?pageID=${numberPage}`);
+    }
+    else{
+        new Error('Error');
+    }
+}
+
+function getURLIdPage() {
+    const url = new URL("../index.html" , window.location);
+    return url.searchParams.get("pageID");
 }
 
 function Init() {
     const listElements = document.getElementById('list');
-    listElements.addEventListener('click', event => checkBoxListener(event.target));
+    const paganationBar = document.getElementById('pgnt');
+    const numberPage = getURLIdPage() || 0;
+
+    document.body.addEventListener('click', event => checkBoxListener(event.target));
     document.getElementById('btnBasic').addEventListener('click', checkAuth);
-    document.getElementById('clearBtn').addEventListener('click', event => clearList(listElements));
-    document.getElementById('sortByNameBtn').addEventListener('click', event => sortElements(event.target, listElements, 'title'));
-    document.getElementById('sortByAlcoBtn').addEventListener('click', event => sortElements(event.target, listElements, 'alcohol'));
+    paganationBar.addEventListener('click', event => createPagination(event.target, listElements, paganationBar));
+    //document.getElementById('clearBtn').addEventListener('click', event => clearList(listElements));
+    document.getElementById('sortByNameBtn').addEventListener('click', event => sortTitleElements(listElements, 'title'));
+    document.getElementById('sortByAlcoBtn').addEventListener('click', event => sortABVElements(listElements, 'alcohol'));
     document.getElementById('showModalWindow').addEventListener('click', event => showModalWindow(listElements));
 
+    getCheckedFromLS();
     renderList();
+    showNumbersOfPages(paganationBar, numberPage);
+    updateUrl(numberPage);
 }
+
+
+// var url = new URL(window.location.href);
+// var c = url.searchParams.get("pageID");
+// console.log(c)
+// let url = window.location.protocol + "//" + window.location.host + window.location.pathname;
+//
